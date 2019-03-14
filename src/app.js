df1db9e5d2dd0a6fb06edc7de61b8c6312bfccd1
Raphael Narasaki <raphael.narasaki@iteris.com.br>
2019-03-14 18:35:46 -0300

    init app

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { CssBaseline } from '@material-ui/core'
+
+import { MainPage } from './pages/main'
+import { Login } from './pages/login'
+
+const App = () => (
+  <Fragment>
+    <CssBaseline />
+    <BrowserRouter>
+      <Switch>
+        <Route path='/login' component={Login} exact />
+        <Route component={MainPage} />
+      </Switch>
+    </BrowserRouter>
+  </Fragment>
+)
+
+export default App
---
+++ src/pages/login/login.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import { Button, Grid } from '@material-ui/core'
+
+const config = {
+  apiKey: 'AIzaSyDwojIWCv5Sj6ARbNxXmnmYbWX-FQpBBL8',
+  authDomain: 'reactzzaria-af092.firebaseapp.com',
+  databaseURL: 'https://reactzzaria-af092.firebaseio.com',
+  projectId: 'reactzzaria-af092',
+  storageBucket: 'reactzzaria-af092.appspot.com',
+  messagingSenderId: '589648798408'
+}
+
+firebase.initializeApp(config)
+
+class Login extends Component {
+  state = {
+    isUserLoggedIn: false,
+    user: null
+  }
+
+  componentDidMount () {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        console.log('user: ', user)
+        this.setState({
+          isUserLoggedIn: true,
+          user
+        })
+      } else {
+        console.log('nao eh usuario::', user)
+        this.setState({
+          isUserLoggedIn: false,
+          user: null
+        })
+      }
+    })
+  }
+
+  login = () => {
+    const provider = new firebase.auth.GithubAuthProvider()
+    firebase.auth().signInWithRedirect(provider)
+  }
+
+  logout = () => {
+    firebase.auth().signOut().then(() => {
+      console.log('deslogou')
+      this.setState({
+        isUserLoggedIn: false,
+        user: null
+      })
+    })
+  }
+
+  render () {
+    const { isUserLoggedIn, user } = this.state
+
+    return (
+      <Container>
+        <Grid container spacing={24} justify='center'>
+          <Grid item xs={6}>
+            <Logo>Logo</Logo>
+          </Grid>
+          <Grid item xs={12} container justify='center'>
+            {
+              isUserLoggedIn && (
+                <div>
+                  <pre>{user.displayName}</pre>
+                  <Button variant='contained' onClick={this.logout}>Sair</Button>
+                </div>
+              )
+            }
+            {
+              !isUserLoggedIn && <GitHubButton onClick={this.login}>Entrar com Github</GitHubButton>
+            }
+
+          </Grid>
+        </Grid>
+      </Container>
+    )
+  }
+}
+
+const Container = styled.div`
+  padding: 20px
+`
+
+const Logo = styled.h1`
+  width: 100%;
+  text-align: center;
+`
+
+const GitHubButton = styled(Button).attrs({
+  variant: 'contained',
+  fullWidth: true
+})`
+  &&{
+    font-size: 20px;
+    max-width: 480px;
+    padding: 10px;
+    text-transform: none;
+  }
+`
+
+export default Login
